0fcfa5ef2042d54113d7624e60bc5cb3a15b3b96
ivje9902 <Ivjen02@gmail.com>
2024-04-18 14:04:51 +0200

    Co-authored-by: maxeby <maxeby@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, addDoc, getDocs, query, where, doc, setDoc, updateDoc, deleteDoc } from 'firebase/firestore';
+import { getFirestore, collection, addDoc, getDocs, query, where, doc, setDoc, updateDoc, deleteDoc, count, get, getDoc } from 'firebase/firestore';
+import { getStorage, ref, exists, getDownloadURL, uploadString, uploadBytes } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAjWnGy2HpTFM-07fRp3VIokULmU_dyMg4",
@@ -16,11 +17,26 @@ const firebaseApp = initializeApp(firebaseConfig);
 
 const db = getFirestore();
 
+const storage = getStorage();
+
+async function getFileDownloadURL(path) {
+  try {
+    const fileRef = ref(storage, path);
+    const downloadURL = await getDownloadURL(fileRef);
+    console.log("Download URL:", downloadURL);
+    return downloadURL;
+  } catch (error) {
+    console.error("Error getting download URL:", error);
+    return null;
+  }
+}
+
 
 async function getCourses() {
   const snapshot = await getDocs(collection(db, "kurser"));
   return snapshot.docs.map(doc => doc.data());
 }
+
 
 async function generateCourseNavigation() {
   const courses = await getCourses();
@@ -46,7 +62,8 @@ async function generateCourseNavigation() {
     button.addEventListener('click', () => {
       window.location.href = 'lectures.html';
       localStorage.setItem("course", course.name);
-    });    // button.style.width = "20rem";
+    });
+    // button.style.width = "20rem";
 
     //const div_container = document.createElement("div");
     //div_container.classList.add("card", "container", "mt-5");
@@ -54,8 +71,6 @@ async function generateCourseNavigation() {
 
     // const div_body = document.createElement("div");
     // div_body.classList.add("card-body");
-
-
 
     h5.appendChild(link);
     button.appendChild(h5);
@@ -65,15 +80,69 @@ async function generateCourseNavigation() {
     // div_container.appendChild(div_body);
     //nav.appendChild(div_container);
     nav.style.width = "100%";
-
-
   });
+}
+
+async function generateCourseExams() {
+
+  var courseID = localStorage.getItem("ID");
+  console.log(courseID);
+
+  const docRef = doc(db, "1MS005", "Tentor");
+  const docSnap = await getDoc(docRef);
+  console.log(docSnap);
+  const row = document.getElementById("exams");
+
+
+  if (docSnap) {
+    const data = docSnap.data();
+    if (data) {
+      // Iterate over each field in the document's data
+      for (const fieldName in data) {
+
+        const arrayField = data[fieldName];
+        console.log(arrayField[0]);
+
+        const card_container = document.createElement("div");
+        card_container.classList.add("card", "container", "mt-5");
+        card_container.style.width = "20rem";
+
+
+        const card_body = document.createElement("div");
+        card_body.classList.add("card-body");
+
+
+        const name = document.createElement("h5");
+        name.textContent = arrayField[0];
+
+        const button = document.createElement("button");
+        const link = getFileDownloadURL(arrayField[1]);
+        console.log(link);
+        button.textContent = "Click to open";
+        button.classList.add("btn", "btn-primary", "btn-lg");
+        button.addEventListener('click', () => {
+          window.open(link);
+        });
+
+        card_body.appendChild(name);
+        card_body.appendChild(button);
+        card_container.appendChild(card_body);
+        row.appendChild(card_container);
+
+
+      }
+    }
+  } else {
+    console.log("No such document!");
+  }
+
+
 }
 
 
 
 
-
+window.generateCourseExams = generateCourseExams;
 window.generateCourseNavigation = generateCourseNavigation;
 
 
